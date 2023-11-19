1ce6cf71ab3d3fead054397f2dd1bc7d55b7245f
milanhirani27 <milan.170410116023@gmail.com>
2023-11-20 04:36:36 +0530

    changed index.js file

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/**
- * @format
- */
+import ChatComponent from './src/components/ChatComponent';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+export { ChatComponent };
