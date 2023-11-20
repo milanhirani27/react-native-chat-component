b2acfacd731d6fe2cabf6d7284125137ce98439d
milanhirani27 <milan.170410116023@gmail.com>
2023-11-20 13:48:10 +0530

    fixed props issue

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
 import ChatComponent from './src/components/ChatComponent';
 
 const App = () => {
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ChatComponent />
-    </SafeAreaView>
+      <ChatComponent headerColor='#3498db' headerTitle='Chat Room'c/> 
   );
 };
 
